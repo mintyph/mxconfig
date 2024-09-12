b27ed6e5e16345c4bfbdd3e46fa746b48031195e
Mint <127717019+mintyph@users.noreply.github.com>
2024-09-12 15:24:17 -0300

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
 import time
 import subprocess
+import sys
 
 def print_banner():
     banner = """
 ⠄⠄⠄⠄⢠⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣯⢻⣿⣿⣿⣿⣆⠄⠄⠄
-⠄⠄⣼⢀⣿⣿⣿⣿⣏⡏⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿⣧⢻⣿⣿⣿⣿⡆⠄⠄
-⠄⠄⡟⣼⣿⣿⣿⣿⣿⠄⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⣿⣇⢻⣿⣿⣿⣿⠄⠄
-⠄⢰⠃⣿⣿⠿⣿⣿⣿⠄⠄⠄⠄⠄⠄⠙⠿⣿⣿⣿⣿⣿⠄⢿⣿⣿⣿⡄⠄
-⠄⢸⢠⣿⣿⣧⡙⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠈⠛⢿⣿⣿⡇⠸⣿⡿⣸⡇⠄
-⠄⠈⡆⣿⣿⣿⣿⣦⡙⠳⠄⠄⠄⠄⠄⠄⢀⣠⣤⣀⣈⠙⠃⠄⠿⢇⣿⡇⠄
+⠄⠄⣼⢀⣿⣿⣿⣿⣏⡏⠄⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿⣧⢻⣿⣿⣿⣿⡆⠄⠄
+⠄⠄⡟⣼⣿⣿⣿⣿⣿⠄⠄⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⣿⣇⢻⣿⣿⣿⣿⠄⠄
+⠄⢰⠃⣿⣿⠿⣿⣿⣿⠄⠄⠄⠄⠄⠄⠄⠙⠿⣿⣿⣿⣿⣿⠄⢿⣿⣿⣿⡄⠄
+⠄⢸⢠⣿⣿⣧⡙⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠈⠛⢿⣿⣿⡇⠸⣿⡿⣸⡇⠄
+⠄⠈⡆⣿⣿⣿⣿⣦⡙⠳⠄⠄⠄⠄⠄⠄⢀⣠⣤⣀⣈⠙⠃⠄⠄⠿⢇⣿⡇⠄
 ⠄⠄⡇⢿⣿⣿⣿⣿⡇⠄⠄⠄⠄⠄⣠⣶⣿⣿⣿⣿⣿⣿⣷⣆⡀⣼⣿⡇⠄
 ⠄⠄⢹⡘⣿⣿⣿⢿⣷⡀⠄⢀⣴⣾⣟⠉⠉⠉⠉⣽⣿⣿⣿⣿⠇⢹⣿⠃⠄
 ⠄⠄⠄⢷⡘⢿⣿⣎⢻⣷⠰⣿⣿⣿⣿⣦⣀⣀⣴⣿⣿⣿⠟⢫⡾⢸⡟⠄.
@@ -20,11 +21,22 @@ def print_banner():
     print(banner)
     time.sleep(3)
 
+def check_sudo():
+    if os.geteuid() != 0:
+        print("This script must be run as root.")
+        sys.exit(1)
+
+def install_vscodium():
+    print("Installing VSCodium...")
+    subprocess.run(["wget", "https://github.com/VSCodium/vscodium/releases/download/1.71.0.0/codium_1.71.0.0_amd64.deb"], check=True)
+    subprocess.run(["sudo", "dpkg", "-i", "codium_1.71.0.0_amd64.deb"], check=True)
+    subprocess.run(["sudo", "apt", "-f", "install", "-y"], check=True)
+
 def install_packages():
-    home_dir = os.path.expanduser("~")  # Getting Home Dir
+    home_dir = os.path.expanduser("~")
 
     print("Installing tmux...")
-    subprocess.run(["sudo", "apt", "install", "-y", "tmux"])
+    subprocess.run(["sudo", "apt", "install", "-y", "tmux"], check=True)
 
     print("Configuring tmux...")
     tmux_config = """
@@ -38,21 +50,23 @@ def install_packages():
         f.write(tmux_config)
 
     print("Setting up tmux to start automatically...")
-    subprocess.run("echo 'tmux' >> ~/.bashrc", shell=True)
+    if not os.system("grep -qxF 'tmux' ~/.bashrc"):
+        with open(f"{home_dir}/.bashrc", "a") as f:
+            f.write("tmux\n")
 
     print("Updating and upgrading system...")
-    subprocess.run(["sudo", "apt", "update", "-y"])
-    subprocess.run(["sudo", "apt", "upgrade", "-y"])
+    subprocess.run(["sudo", "apt", "update", "-y"], check=True)
+    subprocess.run(["sudo", "apt", "upgrade", "-y"], check=True)
 
     print("Installing burpsuite...")
-    subprocess.run(["sudo", "apt", "install", "-y", "burpsuite"])
+    subprocess.run(["sudo", "apt", "install", "-y", "burpsuite"], check=True)
 
-    print("Installing VSCodium...")
-    subprocess.run(["sudo", "apt", "install", "-y", "vscodium"])
+    install_vscodium()
 
     print("All tasks completed!")
 
 def main():
+    check_sudo()
     print_banner()
     install_packages()
 
