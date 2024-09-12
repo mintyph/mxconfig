9f65ce2f889da7c6800a4794609f68e091bf2b86
Mint <127717019+mintyph@users.noreply.github.com>
2024-09-12 15:22:46 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import time
+import subprocess
+
+def print_banner():
+    banner = """
+⠄⠄⠄⠄⢠⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣯⢻⣿⣿⣿⣿⣆⠄⠄⠄
+⠄⠄⣼⢀⣿⣿⣿⣿⣏⡏⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿⣧⢻⣿⣿⣿⣿⡆⠄⠄
+⠄⠄⡟⣼⣿⣿⣿⣿⣿⠄⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⣿⣇⢻⣿⣿⣿⣿⠄⠄
+⠄⢰⠃⣿⣿⠿⣿⣿⣿⠄⠄⠄⠄⠄⠄⠙⠿⣿⣿⣿⣿⣿⠄⢿⣿⣿⣿⡄⠄
+⠄⢸⢠⣿⣿⣧⡙⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠈⠛⢿⣿⣿⡇⠸⣿⡿⣸⡇⠄
+⠄⠈⡆⣿⣿⣿⣿⣦⡙⠳⠄⠄⠄⠄⠄⠄⢀⣠⣤⣀⣈⠙⠃⠄⠿⢇⣿⡇⠄
+⠄⠄⡇⢿⣿⣿⣿⣿⡇⠄⠄⠄⠄⠄⣠⣶⣿⣿⣿⣿⣿⣿⣷⣆⡀⣼⣿⡇⠄
+⠄⠄⢹⡘⣿⣿⣿⢿⣷⡀⠄⢀⣴⣾⣟⠉⠉⠉⠉⣽⣿⣿⣿⣿⠇⢹⣿⠃⠄
+⠄⠄⠄⢷⡘⢿⣿⣎⢻⣷⠰⣿⣿⣿⣿⣦⣀⣀⣴⣿⣿⣿⠟⢫⡾⢸⡟⠄.
+⠄⠄⠄⠄⠻⣦⡙⠿⣧⠙⢷⠙⠻⠿⢿⡿⠿⠿⠛⠋⠉⠄⠂⠘⠁⠞⠄⠄⠄
+⠄⠄⠄⠄⠄⠈⠙⠑⣠⣤⣴⡖⠄⠿⣋⣉⣉⡁⠄⢾⣦⠄⠄⠄⠄⠄⠄⠄⠄
+    MXNT ParrotOS Settings.
+    """
+    print(banner)
+    time.sleep(3)
+
+def install_packages():
+    home_dir = os.path.expanduser("~")  # Getting Home Dir
+
+    print("Installing tmux...")
+    subprocess.run(["sudo", "apt", "install", "-y", "tmux"])
+
+    print("Configuring tmux...")
+    tmux_config = """
+    # ~/.tmux.conf
+    bind d split-window -h
+    bind w split-window -v
+    bind c copy-mode
+    bind v paste-buffer
+    """
+    with open(f"{home_dir}/.tmux.conf", "w") as f:
+        f.write(tmux_config)
+
+    print("Setting up tmux to start automatically...")
+    subprocess.run("echo 'tmux' >> ~/.bashrc", shell=True)
+
+    print("Updating and upgrading system...")
+    subprocess.run(["sudo", "apt", "update", "-y"])
+    subprocess.run(["sudo", "apt", "upgrade", "-y"])
+
+    print("Installing burpsuite...")
+    subprocess.run(["sudo", "apt", "install", "-y", "burpsuite"])
+
+    print("Installing VSCodium...")
+    subprocess.run(["sudo", "apt", "install", "-y", "vscodium"])
+
+    print("All tasks completed!")
+
+def main():
+    print_banner()
+    install_packages()
+
+if __name__ == "__main__":
+    main()
